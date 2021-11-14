3930d9250ef8494d84c13b68ddb41d4401752a97
youranreus <youranreus@qq.com>
2021-11-14 09:12:45 +0800

    一些个css调整

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
 
     <!-- 使用url函数转换相关路径 -->
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css?v=1.999'); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css?v=2.0.1'); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/youranreus/R@v1.1.0/W/prism.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/youranreus/R@v1.1.0/W/typo.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/youranreus/R@v1.1.0/G/CSS/OwO.min.css">
